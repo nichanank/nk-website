9b0e282b07da0a1cadca155e4c8d5b0b1589e104
nichanank <n.kesonpat@gmail.com>
2018-07-19 17:18:35 +0700

    route fix

--- keystone.js
+++ keystone.js
@@ -16,7 +16,7 @@ keystone.init({
 
 	'sass': 'public',
 	'static': 'public',
-	'favicon': 'public/favicon.ico',
+	'favicon': 'public/images/favicon.ico',
 	'views': 'templates/views',
 	'view engine': '.hbs',
 	'cookie secret': process.env.COOKIE_SECRET,
